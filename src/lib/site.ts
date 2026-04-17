324517336377aadbf640a20cad508cd20acbb064
bearylabs <info@beary.cloud>
2026-04-17 11:35:28 +0200

    Convert site images to WebP

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,3 +1,23 @@
+import type { ImageMetadata } from "astro";
+
+import aboutImage from "@/assets/images/about/DSC09426.jpg";
+import faviconImage from "@/assets/images/favicon.png";
+import heroImage from "@/assets/images/home/Background-2.jpg";
+import sliderImage01 from "@/assets/images/slider/d6c7e556-59d2-4dd2-b107-b5c191f9f90d.jpeg";
+import sliderImage02 from "@/assets/images/slider/d3b1a031-e2d9-4bfa-8798-357080485e16.jpeg";
+import sliderImage03 from "@/assets/images/slider/c6119631-70f9-448f-a849-e4ac752ba680.jpeg";
+import sliderImage04 from "@/assets/images/slider/ad356c3a-a904-414d-b6ab-988368835eef.jpeg";
+import sliderImage05 from "@/assets/images/slider/6874e43f-d679-4570-81ab-f394efe0472f.jpeg";
+import sliderImage06 from "@/assets/images/slider/87d23d2a-6e66-4689-8d41-a98916b50a06.jpeg";
+import sliderImage07 from "@/assets/images/slider/9bfaf503-78fb-4a67-942f-8031602c8fc9.jpeg";
+import sliderImage08 from "@/assets/images/slider/8d8587a9-8b68-47ce-b65c-745be7e40355.jpeg";
+import sliderImage09 from "@/assets/images/slider/7a41e474-ef7b-4b2d-bbef-a17593f4eedf.jpeg";
+import sliderImage10 from "@/assets/images/slider/5a405a97-ae91-408e-a915-a984fafde835.jpeg";
+import sliderImage11 from "@/assets/images/slider/1cdfa965-4934-41af-9122-03b9b169bfae.jpeg";
+import warehouseImage01 from "@/assets/images/warehouse/57-2.jpg";
+import warehouseImage02 from "@/assets/images/warehouse/Aussenansicht.jpg";
+import warehouseImage03 from "@/assets/images/warehouse/57-1.jpg";
+
 export const site = {
   name: "Matratzen Outlet Lintfort",
   title: "Matratzen Outlet | Matratzen zu unschlagbaren Preisen",
@@ -13,9 +33,9 @@ export const site = {
   facebook:
     "https://www.facebook.com/Matratzen-Outlet-Kamp-Lintfort-107765121668884",
   instagram: "https://www.instagram.com/matratzen_outlet_kamp_lintfort/",
-  heroImage: "/assets/images/home/Background-2.jpg",
+  heroImage,
   logo: "/assets/images/logo.svg",
-  favicon: "/assets/images/favicon.png",
+  favicon: faviconImage,
 };
 
 export const openingHours = [
@@ -28,18 +48,18 @@ export const announcement = {
   text: "Eine frische Lieferung mit vielen neuen hochwertigen Matratzen ist neu bei uns eingetroffen",
 };
 
-export const sliderImages = [
-  "/assets/images/slider/d6c7e556-59d2-4dd2-b107-b5c191f9f90d.jpeg",
-  "/assets/images/slider/d3b1a031-e2d9-4bfa-8798-357080485e16.jpeg",
-  "/assets/images/slider/c6119631-70f9-448f-a849-e4ac752ba680.jpeg",
-  "/assets/images/slider/ad356c3a-a904-414d-b6ab-988368835eef.jpeg",
-  "/assets/images/slider/6874e43f-d679-4570-81ab-f394efe0472f.jpeg",
-  "/assets/images/slider/87d23d2a-6e66-4689-8d41-a98916b50a06.jpeg",
-  "/assets/images/slider/9bfaf503-78fb-4a67-942f-8031602c8fc9.jpeg",
-  "/assets/images/slider/8d8587a9-8b68-47ce-b65c-745be7e40355.jpeg",
-  "/assets/images/slider/7a41e474-ef7b-4b2d-bbef-a17593f4eedf.jpeg",
-  "/assets/images/slider/5a405a97-ae91-408e-a915-a984fafde835.jpeg",
-  "/assets/images/slider/1cdfa965-4934-41af-9122-03b9b169bfae.jpeg",
+export const sliderImages: ImageMetadata[] = [
+  sliderImage01,
+  sliderImage02,
+  sliderImage03,
+  sliderImage04,
+  sliderImage05,
+  sliderImage06,
+  sliderImage07,
+  sliderImage08,
+  sliderImage09,
+  sliderImage10,
+  sliderImage11,
 ];
 
 export const about = {
@@ -50,7 +70,7 @@ export const about = {
     "Bei uns gibt es Matratzen und weitere Bettwaren zum günstigsten Preis. Komm doch einfach zum Probeliegen vorbei.",
     "Wir freuen uns auf Dich!",
   ],
-  image: "/assets/images/about/DSC09426.jpg",
+  image: aboutImage,
   imageAlt: "Die beiden Brüder und Geschäftsführer Tristan und Leo Hennen",
 };
 
@@ -61,15 +81,15 @@ export const warehouse = {
     "Auf über 500 Quadratmeter bieten wir eine Auswahl von mehr als 800 Matratzen, sowie Topper, Lattenroste und Betten.",
   images: [
     {
-      src: "/assets/images/warehouse/57-2.jpg",
+      src: warehouseImage01,
       alt: "Matratzen im Lager des Matratzen Outlet Lintfort",
     },
     {
-      src: "/assets/images/warehouse/Aussenansicht.jpg",
+      src: warehouseImage02,
       alt: "Eingang vom Büro des Matratzen Outlet Lintfort",
     },
     {
-      src: "/assets/images/warehouse/57-1.jpg",
+      src: warehouseImage03,
       alt: "Weitere Matratzen im Lager",
     },
   ],
